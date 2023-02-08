8e0af31a4611c63b06814b720ccd9a4fc95399b4
Austin Ly <al044516@ohio.edu>
2023-02-08 18:24:02 -0500

    Added k-values for k-means

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from tabulate import tabulate
-
+import pprint
+import csv
 
 
 data = pd.read_csv(
     'GSE64881_segmentation_at_30000bp.passqc.multibam.txt', delimiter="\t")
 # Read only the nps and columns, eliminates unwanted data
-nps = data.iloc[:, 3:]
-sum_columns = nps.sum() #produce a series of the sum of columns
-sum_rows = nps.sum(axis=1) #produce a series of the rows of columns
-
+nps = data.iloc[:, 3:]  # read all rows, start reading from column 3 on
+sum_columns = nps.sum()  # produce a series of the sum of columns
+sum_rows = nps.sum(axis=1)  # produce a series of the rows of columns
 # Isolation of Hist1 region
-hist1_df = data.iloc[69714:69795].copy() #Grab only the hist1 region and create a new dataframe
+# Grab only the hist1 region and create a new dataframe
+hist1_df = data.iloc[69714:69795].copy()
+# read all rows, start reading from column 3 on
 nps_hist_1 = hist1_df.iloc[:, 3:]
-hist1_sum_columns = nps_hist_1.sum()
-hist1_sum_rows = nps_hist_1.sum(axis=1)
-#Isolate relevant Nps
-#creates new dataframe by using boolean indexing(ex. df = df[specified values])
-#return columns where the series value is not equal to zero (ex. series_sum[series_sum != 0])
-#.index returns the column names to form the new data frame
+hist1_sum_columns = nps_hist_1.sum()  # produce a series of the sum of columns
+hist1_sum_rows = nps_hist_1.sum(axis=1)  # produce a series of the sum of rows
+# Isolate relevant Nps
+# creates new dataframe by using boolean indexing(ex. df = df[specified values])
+# return columns where the series value is not equal to zero (ex. series_sum[series_sum != 0])
+# .index returns the column names to form the new data frame
 nps_hist_1 = nps_hist_1[hist1_sum_columns[hist1_sum_columns != 0].index]
-#create a new series of sums for the isolated nps
+# create a new series of sums for the isolated nps
 hist1_sum_columns = nps_hist_1.sum()
-print(nps_hist_1)
-
 
 
 def num_windows():
@@ -44,8 +45,8 @@ def count_npfs():
 
 
 def average_num_windows_per_np():
-    count = sum_columns.sum()
-    return count / num_windows()
+    count = sum_columns.sum()  # gets the full number of detections
+    return count / num_windows()  # num windows per np
 
 
 def smallest_num_windows_in_np():
@@ -244,18 +245,22 @@ def hist1_compaction():
 
     print("Average compaction: ", average_compaction)
 
-#Hist1 Jaccard index
+# Hist1 Jaccard index
+
 
 def store_value():
-    #Create a dictionary of arrays with a key value
+    # Create a dictionary of arrays with a key value
     nps_arrays = {}
     for column in nps_hist_1.columns:
-        nps_array = nps_hist_1[column].to_numpy() #convert each column to its own array
-        nps_arrays[column] = nps_array  #push back the newly formed array into the dictionary
+        # convert each column to its own array
+        nps_array = nps_hist_1[column].to_numpy()
+        # push back the newly formed array into the dictionary
+        nps_arrays[column] = nps_array
 
-    return nps_arrays    
-        
-def hist1_jaccard_index(np1: np.ndarray, np2:np.ndarray):
+    return nps_arrays
+
+
+def hist1_jaccard_index(np1: np.ndarray, np2: np.ndarray):
     m11 = 0
     m01 = 0
     m10 = 0
@@ -266,21 +271,124 @@ def hist1_jaccard_index(np1: np.ndarray, np2:np.ndarray):
             m01 += 1
         elif np1[index] == 1 and np2[index] == 0:
             m10 += 1
-    
-    
+
     jaccard_index = m11 / (m11 + m10 + m01)
     return jaccard_index
 
+
 def store_jaccard_index(nps_arrays: dict):
-    jacard_matrix = pd.DataFrame(index=nps_arrays.keys(), columns=nps_arrays.keys())
+    jacard_matrix = pd.DataFrame(
+        index=nps_arrays.keys(), columns=nps_arrays.keys())
     for x in jacard_matrix.columns:
         for y in jacard_matrix.index:
-            jacard_matrix.at[x,y] = hist1_jaccard_index(nps_arrays[x], nps_arrays[y])
-   
-    
-    jacard_matrix.to_markdown('jaccard_matrix.txt' )
+            jacard_matrix.at[x, y] = hist1_jaccard_index(
+                nps_arrays[x], nps_arrays[y])
 
-#Print the data
+    jacard_matrix = pd.read_csv('jaccard_matrix.csv')
+    print(jacard_matrix)
+
+    sns.heatmap(jacard_matrix)
+    plt.show()
+
+
+def hist1_distance_index(np1: np.ndarray, np2: np.ndarray):
+    m11 = 0
+    m01 = 0
+    m10 = 0
+    for index in range(len(np1)):
+        if np1[index] == 1 and np2[index] == 1:
+            m11 += 1
+        elif np1[index] == 0 and np2[index] == 1:
+            m01 += 1
+        elif np1[index] == 1 and np2[index] == 0:
+            m10 += 1
+
+    jaccard_index = m11 / (m11 + m10 + m01)
+    return 1 - jaccard_index
+
+
+def store_distance_matrix(nps_arrays: dict):
+    distance_matrix = pd.DataFrame(
+        index=nps_arrays.keys(), columns=nps_arrays.keys())
+    for x in distance_matrix.columns:
+        for y in distance_matrix.index:
+            distance_matrix.at[x, y] = hist1_distance_index(
+                nps_arrays[x], nps_arrays[y])
+
+    distance_matrix = pd.read_csv('distance_matrix.csv')
+    print(distance_matrix)
+    sns.heatmap(distance_matrix)
+    plt.show()
+
+
+def normalize_jaccard_index(np1: np.ndarray, np2: np.ndarray):
+    m11 = 0
+    m01 = 0
+    m10 = 0
+    for index in range(len(np1)):
+        if np1[index] == 1 and np2[index] == 1:
+            m11 += 1
+        elif np1[index] == 0 and np2[index] == 1:
+            m01 += 1
+        elif np1[index] == 1 and np2[index] == 0:
+            m10 += 1
+
+    jaccard_index = m11 / min(m10 + m11, m01 + m11)
+    return jaccard_index
+
+
+def store_normalize_jaccard_matrix(nps_arrays: dict):
+    normalize_jaccard_matrix = pd.DataFrame(
+        index=nps_arrays.keys(), columns=nps_arrays.keys())
+    for x in normalize_jaccard_matrix.columns:
+        for y in normalize_jaccard_matrix.index:
+            normalize_jaccard_matrix.at[x, y] = normalize_jaccard_index(
+                nps_arrays[x], nps_arrays[y])
+
+    normalize_jaccard_matrix = pd.read_csv('normalize_jaccard_matrix.csv')
+    print(normalize_jaccard_matrix)
+    sns.heatmap(normalize_jaccard_matrix)
+    plt.show()
+
+
+# For Heat Maps
+# Stores each NP detections into a dictionary (Ex. [0,1,1,0,0,1,0], [0,1,0,0,0,0,1]) with the key being the specific NP
+nps_arrays = store_value()
+
+
+def select_k_value():
+    normalize_df = pd.read_csv('normalize_jaccard_matrix.csv')
+    normalize_df = normalize_df.sample(n=3, replace=True)
+    print(normalize_df)
+    c1 = []
+    c2 = []
+    c3 = []
+    for x in normalize_df.columns:
+        column = normalize_df[x]
+        min_value = column.min()
+        # If there are duplicates in the colum
+        duplicates = column[column == min_value]
+        if len(duplicates) > 1:
+            min_index = np.random.choice(duplicates.index)
+        else:
+            min_index = column.idxmin()
+
+        if min_index == normalize_df.index[0]:
+            c1.append(x)
+
+        if min_index == normalize_df.index[1]:
+            c2.append(x)
+
+        if min_index == normalize_df.index[2]:
+            c3.append(x)
+
+    np_relation = {
+        normalize_df.index[0]: c1, normalize_df.index[1]: c2, normalize_df.index[2]: c3}
+    pprint.pprint(np_relation)
+
+
+"""
+# Print the data
 print("-----------------------------------------------------------------------")
 print(data)
 print("DATA for Activity 1")
@@ -329,10 +437,23 @@ hist1_compaction()
 print(" Data for Activity 4")
 print("------------------------------------------------------------------------")
 print("------------------------------------------------------------------------")
+nps_arrays = store_value()
 print("Jaccard Matrix")
-nps_arrays = store_value()  
+print("________________________________________________________________________")
 store_jaccard_index(nps_arrays)
-print("Check jaccard_index.txt")
+print("------------------------------------------------------------")
+print("Difference Matrix")
+store_distance_matrix(nps_arrays)
+
+print("-----------------------------------------------------------------------_")
+print("Activity 5: Clustering")
+print("------------------------------------------------------------------------")
+print("Normalized Jaccard Distance Matrix")
+store_normalize_jaccard_matrix(nps_arrays)
+"""
+select_k_value()
 quit()
-#Is the HLB in different state? (Not formed, forming, formed)
-#To determine this we must find difference and similarity between samples. We need to find three different states of these NPS
+
+
+# Is the HLB in different state? (Not formed, forming, formed)
+# To determine this we must find difference and similarity between samples. We need to find three different states of these NPS
